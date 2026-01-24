5400e26aea0367894f199b8ae7604fe04b78e235
wanyi <liuyudan98@163.com>
2026-01-24 15:23:19 +0800

    feat: github部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  output: 'static',
+  site: 'https://Wanyi9988.github.io', // 先写这个，后面解释
+  base: '/BLOG/',                     // 如果不是用户主页仓库
 });
