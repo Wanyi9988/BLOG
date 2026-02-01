aac930611efe73b058db8363c1c3d9389e863b92
wanyi <liuyudan98@163.com>
2026-02-01 14:15:53 +0800

    feat: 关于复杂动力学第一定律

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,17 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   site: 'https://Wanyi9988.github.io', // 先写这个，后面解释
   base: '/BLOG/',                     // 如果不是用户主页仓库
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
